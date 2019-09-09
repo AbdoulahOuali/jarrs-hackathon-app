45ccebdacfbab78bc6563317b6dfae14511e6ea7
Abdoulah Ouali <32765173+MocroTronics@users.noreply.github.com>
2019-09-09 15:44:12 +0200

    improved splash-screen

--- app/src/main/java/com/ingenico/epayments/jarrs/hackaton/oqp/SplashScreenActivity.java
+++ app/src/main/java/com/ingenico/epayments/jarrs/hackaton/oqp/SplashScreenActivity.java
@@ -1,6 +1,7 @@
 package com.ingenico.epayments.jarrs.hackaton.oqp;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,18 +19,22 @@ public class SplashScreenActivity extends AppCompatActivity {
                 .withTargetActivity(MainActivity.class)
                 .withSplashTimeOut(5000)
                 .withHeaderText("Seemless Offline Payments")
-                .withFooterText("JARRS Hackaton team" + "\u00A9")
+                .withFooterText("JARRS Hackaton team 2019" + "\u00A9")
                 .withBeforeLogoText("Powered by")
                 .withAfterLogoText("Shaping the Future of Payments")
                 .withLogo(R.drawable.ic_brand_logo)
                 .withBackgroundResource(android.R.color.background_light);
 
+        configSplash.getHeaderTextView().setTextSize(36);
+        configSplash.getHeaderTextView().setGravity(Gravity.TOP | Gravity.CENTER);
         configSplash.getHeaderTextView().setPadding(24, 200, 24, 24);
         configSplash.getFooterTextView().setPadding(24, 24, 24, 100);
         configSplash.getHeaderTextView().setTextColor(getResources().getColor(R.color.ingenicoBlue));
         configSplash.getFooterTextView().setTextColor(getResources().getColor(R.color.ingenicoBlue));
         configSplash.getBeforeLogoTextView().setTextColor(getResources().getColor(R.color.ingenicoBlue));
+        configSplash.getBeforeLogoTextView().setPadding(0, 0, 0, 24);
         configSplash.getAfterLogoTextView().setTextColor(getResources().getColor(R.color.ingenicoBlue));
+        configSplash.getAfterLogoTextView().setPadding(0, 24, 0, 0);
         configSplash.getHeaderTextView().setTextColor(getResources().getColor(R.color.ingenicoBlue));
 
         View easySplashScreen = configSplash.create();
